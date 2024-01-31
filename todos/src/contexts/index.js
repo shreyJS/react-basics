0d82a52cb1e4e0f13709ee1be494336697a30677
shreyJS <deshpandeshreyas850@gmail.com>
2024-01-31 17:20:14 +0530

    todos app

---
+++ todos/src/contexts/index.js
@@ -0,0 +1,9 @@
+export {TodoContext, TodoProvider, useTodo} from './TodoContext'
+
+
+
+/** Method for using context
+ * 1. create context file and create > use > provide context
+ * 2. wrap main component file (usually App.jsx) with provider
+ * 3. 
+ */
